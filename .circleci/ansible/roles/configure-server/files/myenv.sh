d47b06c548568e8c22d2c7b89c0b2681db906776
dravellar <fredricksimi@proton.me>
2023-04-13 13:34:42 +0300

    new commit

---
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -0,0 +1,6 @@
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_HOST=database-1.cxnnfaozmo6r.us-east-1.rds.amazonaws.com
+export TYPEORM_PORT=5432
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=postgres
+export TYPEORM_DATABASE=postgres
